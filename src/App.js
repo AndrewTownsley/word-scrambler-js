ccbaafd2abdc36b82ce66e84f52784243e44cab1
Andrew TOwnsley <atownsley86@gmail.com>
2022-02-28 22:06:18 -0700

    number counter to fetch next sentence is working.  Next step is to scramble the sentence.

--- src/App.js
+++ src/App.js
@@ -4,24 +4,34 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [sentence, setSentence] = useState([]);
+  const [nextNumber, setNextNumber] = useState(1);
 
   const fetchSentence = async () => {
-   await fetch(`https://api.hatchways.io/assessment/sentences/1`)
+   await fetch(`https://api.hatchways.io/assessment/sentences/${nextNumber}`)
     .then(res => res.json())
     .then(data => setSentence(data.data.sentence))
   }
 
-
   useEffect(() => {
     fetchSentence()
     console.log(sentence);
-  }, [sentence])
+  }, [sentence, nextNumber])
+
+  const fetchNumberCounter = () => {
+      if(nextNumber < 10) {
+        setNextNumber(nextNumber + 1)
+      } else {
+        setNextNumber(1)
+      }
+  }
   
   return (
     <div className="App">
         <section>
           {sentence}
         </section>
+        <p>{nextNumber}</p>
+        <button onClick={fetchNumberCounter}>Next</button>
     </div>
   );
 }
