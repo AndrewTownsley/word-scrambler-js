5ce06a044783ac17ec6f7c5cc9a8249f08a15a49
Andrew TOwnsley <atownsley86@gmail.com>
2022-03-01 22:37:58 -0700

    Sentence is being fetched, split into seperate words, the words into letters, and the letters scrambled.  Next need to scramble the letters according to the instructions.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,29 @@ function App() {
   const [sentence, setSentence] = useState([]);
   const [nextNumber, setNextNumber] = useState(1);
   const [shuffledSentence, setShuffledSentence] = useState('')
-
+  
   const fetchSentence = async () => {
-   await fetch(`https://api.hatchways.io/assessment/sentences/${nextNumber}`)
+    await fetch(`https://api.hatchways.io/assessment/sentences/${nextNumber}`)
     .then(res => res.json())
     .then(data => setSentence(data.data.sentence))
+    // .then(data => console.log(data.data.sentence))
+    // let splitWords = sentence.split(' ')
+    // let splitLetters = shuffle(splitWords.map((word) => word.split('')))
+    // setShuffledSentence(splitWords)
+    // console.log(sentence);
   }
-
-  useEffect(() => {
-    fetchSentence()
-    setShuffledSentence(shuffle(sentence.split('')))
-    console.log(sentence);
-  }, [sentence, nextNumber])
-
-  // useEffect(() => {
-  //   setShuffledSentence(shuffle(sentence.split('')))
-    console.log(shuffledSentence);
-  // }, [setSentence, sentence, shuffledSentence])
-
+  
+  const splitSentence = () => {
+    let splitWords = sentence.split(' ')
+    let splitLetters = shuffle(splitWords.map((word) => shuffle(word.split(''))))
+    setShuffledSentence(splitLetters)
+    console.log("splitLetters:", splitLetters);
+    }
+    
+    useEffect(() => {
+      fetchSentence();
+      splitSentence()
+    }, [sentence, setSentence, nextNumber])
   
   const shuffle = (a) => {
     for (let i = a.length - 1; i > 0; i--) {
@@ -40,13 +45,12 @@ function App() {
         setNextNumber(1)
       }
   }
-// console.log(shuffle(sentence.split('')));
   
   return (
     <div className="App">
         <section>
           {shuffledSentence}
-          {/* {sentence} */}
+          {/* <p>Sentence: </p>{sentence} */}
         </section>
         <button onClick={fetchNumberCounter}>Next</button>
     </div>
