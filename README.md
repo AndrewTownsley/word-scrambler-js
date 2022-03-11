cd9a63026ac4a29ffc6033d65b7de032eba132a7
Andrew TOwnsley <atownsley86@gmail.com>
2022-03-10 22:31:01 -0700

    struggling...:(

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 1.  Error and Loading states.
 2.  Check code against React best practices
 3.  Testing
+
+Word Scrambler Functionality
+
+1.  Begin a new line after each SPACE in the sentence.
+2.  Auto-focus on the next input if the correct letter is input.
+3.  If the sentence is guessed correctly, increase core by 1, and offer option to begin new game with a sentence.
--- src/components/Keys.jsx
+++ src/components/Keys.jsx
@@ -1,52 +1,46 @@
 import React, { useEffect, useState} from 'react'
+import { nanoid } from 'nanoid'
 
-const Keys = ({ sentence, keyArray, setKeyArray }) => {
+const Keys = ({ sentence }) => {
     const [keyMatch, setKeyMatch] = useState(false)
-    // const [keyArray, setKeyArray] = useState([])
+    const [keyArray, setKeyArray] = useState([])
     let keyChars = sentence.split('');
-    const keyCharToObj = keyChars.map((item) => {
-        return { match: false, name: item}
-    })
 
-    const mapObj = keyCharToObj.map((obj) => (
-        console.log(obj.name)
-    ))
-    // console.log(mapObj);
+    useEffect(() => {
+        let keyObjectArray = keyChars.map((item) => {
+            return { keyMatch: false, name: item}
+        })
+        setKeyArray(keyObjectArray)
+    }, [sentence])
+    console.log("keyArray:", keyArray);
 
-        console.log("keyToObj:",keyCharToObj);
-
-    // useEffect(() => {
-    //     const keyCharToObj = () => {
-    //         const newKeyChars = [...keyChars]
-    //         newKeyChars.map((item) => {
-    //             return {...item, match: false}
-    //         })
-    //         setKeyArray(newKeyChars)
-    //         console.log(keyArray);
-    //     }
-    //     keyCharToObj()
-    // }, [keyArray])
-    //     console.log("keyArray:",keyArray);
-    
-    // const checkKeyInput = (i, e, keyItem) => {
-    //     const newKeyChars = [...keyChars];
-    //     if(e.target.value === keyItem) {
-    //         setKeyMatch(true)
-    //         newKeyChars[i].keyMatch = !newKeyChars[i].keyMatch;
-    //         keyChars = [...newKeyChars]
-    //     }
-    // }
-
-//     const handleChange = (e, i) => {
-//         // when input equals maxLength of the input, autofocus the next input
-//         const { maxLength, value } = e.target;
-//         if(value.length === maxLength) {
-//             const nextSibling = document.querySelector(`input[${Number(i) + 1}]`);
-//             if(nextSibling !== null) {
-//                 nextSibling.focus();
-//             }
-//         }
+// const checkKeyInput = (e, keyItem) => {
+//     if(keyItem.name === e.target.value) {
+//         setKeyMatch(true)
+//     }
 // }
+
+// const matchKey = (e, i, keyItem) => {
+//     if(keyItem.name === e.target.value) {
+//         let newKeyItem = {
+//             ...keyItem,
+//             keyMatch: !keyItem.keyMatch
+//         }
+//         const newKeyArray = [...keyArray]
+//         newKeyArray[i].keyMatch = !newKeyArray[i].keyMatch;
+//         setKeyArray(newKeyArray)
+//     }
+// }
+
+const checkKeyInput = (e, id, keyItem) => {
+    const newKeyArray = keyArray.map((item) => {
+        if(id === e.target.id && keyItem.name === e.target.value) {
+            return {...keyItem, keyMatch: true}
+        }
+        return item;
+    })
+    setKeyArray(newKeyArray)
+  }
 
   return (
     <>
@@ -54,21 +48,22 @@ const Keys = ({ sentence, keyArray, setKeyArray }) => {
         <div className='key-container'>
             {
                 
-                keyCharToObj.map((keyItem, i) => (
-                    <input 
-                        // onChange={(e) => checkKeyInput(i, e, keyItem)}
-                        // onChange={(e, i) => handleChange(e, i)}
-                        onChange={() => console.log(keyItem.match)}
-                        type='text' 
-                        name={i}
-                        maxLength={1} 
-                        className={
-                            !keyMatch ? (keyItem === ' ' ? 'key space' : 'key')
-                            :
-                            (keyItem=== ' ' ? 'key space match' : 'key match')} 
-                        key={i}
-                    >
-                    </input>
+                keyArray.map((keyItem, i) => (
+                        <input 
+                            onChange={(e, id) => checkKeyInput(keyItem)}
+                            // onKeyUp={(e) => matchKey(e, i, keyItem)}
+                            type='text' 
+                            name={i}
+                            id={nanoid()}
+                            maxLength={1} 
+                            autoComplete='off'
+                            className={
+                                !keyMatch ? (keyItem.name === ' ' ? 'key space' : 'key')
+                                :
+                                (keyItem.name === ' ' ? 'key space match' : 'key match')} 
+                            key={i}
+                        >
+                        </input>
                 ))
             }
         </div>
